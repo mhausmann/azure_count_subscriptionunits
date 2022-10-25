dfe9fffa1d6f284c74086c022fbd8b57037ab5a2
tyronwilson <tyron.wilson.36@gmail.com>
2022-10-25 15:09:30 +0100

    Update azure_resources.ps1
    Changes made to try and count on a per subscription account

--- azure_resources.ps1
+++ azure_resources.ps1
@@ -1,10 +1,13 @@
 $total_count = 0
+$sub_count = 0
+
 # Get Azure Subscriptions ID's
 $GroupList = az account list --query '[].id'
 
 # Loop for each Azure Subscription ID
 foreach ($id in $GroupList) {
     echo "ID:" $id
+    $sub_count = 0
     if ($id.length -gt 4) {
         $Id_Comp = $id.substring(2,37).Trim('"', " ")
         az account set --subscription $Id_Comp
@@ -16,6 +19,7 @@ foreach ($id in $GroupList) {
             $vm = $vm - 2
             echo "VirtualMachine Count: $vm"
             $total_count = $total_count + $vm
+            $sub_count = $sub_count + $vm
         }
 
         foreach ($line in Get-Content .\resources.azure.list) {
@@ -28,8 +32,10 @@ foreach ($id in $GroupList) {
                 $res = ($res - 2)/3
                 echo "$line Count: $res"
                 $total_count = $total_count + $res
+                $sub_count = $sub_count + $res
             }
         }
     }
+    echo "Total number of resources used for this subscription: $id is $sub_count"
 }
 echo "Total number of resources in all subscriptions: $total_count"
