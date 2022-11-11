c6281da52724ef8ca7a01d3016c4bc66e1724ff1
Matt Hausmann <mhausmann@users.noreply.github.com>
2022-11-11 12:24:06 +0000

    Added table to present license count

--- azure_resources.ps1
+++ azure_resources.ps1
@@ -1,14 +1,17 @@
 $total_count = 0
+$sub_count = 0
+$hashTable = @{}
+
 # Get Azure Subscriptions ID's
 $GroupList = az account list --query '[].id'
 
 # Loop for each Azure Subscription ID
 foreach ($id in $GroupList) {
-    echo "ID:" $id
+    $sub_count = 0
     if ($id.length -gt 4) {
         $Id_Comp = $id.substring(2,37).Trim('"', " ")
         az account set --subscription $Id_Comp
-        # echo "Current subscription is " $Id_Comp
+        echo "Current subscription is " $Id_Comp
         # use for loop to read all values and ID
         $vm=$(az resource list --resource-type "Microsoft.Compute/virtualmachines" --query "[?contains(id,'$Id_Comp')]" --output table | wc -l)
         if ($vm -gt 1) {
@@ -16,6 +19,7 @@ foreach ($id in $GroupList) {
             $vm = $vm - 2
             echo "VirtualMachine Count: $vm"
             $total_count = $total_count + $vm
+            $sub_count = $sub_count + $vm
         }
 
         foreach ($line in Get-Content .\resources.azure.list) {
@@ -26,10 +30,14 @@ foreach ($id in $GroupList) {
             if ($res -gt 1) {
                 # PaaS services are counted at 1:3 SU's
                 $res = ($res - 2)/3
-                echo "$line Count: $res"
                 $total_count = $total_count + $res
+                $sub_count = $sub_count + $res
             }
         }
+        $hashTable.Add($Id_Comp,$sub_count)
     }
 }
+
+$hashTable
+
 echo "Total number of resources in all subscriptions: $total_count"
